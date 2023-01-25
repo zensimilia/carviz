680265bb40118d55a7727048522f03a089dad0ca
Di M Dub <zensimilia@gmail.com>
2023-01-25 23:59:58 +0300

    Rocket screen WIP

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,25 @@
 #include <esp_wifi.h>
 #include <settings.h>
 #include <i2sadc.h>
+#include <Fonts/FreeSerif9pt7b.h>
+#include "rocket_img.h"
 
-// PAL, 8-bit (RGB332) color
-ESP_8_BIT_GFX videoOut(false, 8);
+// NTSC, 240x256, 8-bit (RGB332) color
+ESP_8_BIT_GFX videoOut(true, 8);
 arduinoFFT FFT = arduinoFFT();
 TaskHandle_t adcWriterTaskHandle;
+
+#define ASTEROIDS 32
+
+short int asteroidsX[ASTEROIDS];
+short int asteroidsY[ASTEROIDS];
+short int asteroidsZ[ASTEROIDS];
+short int asteroidsS[ASTEROIDS];
+
+unsigned char frames = 0;
+unsigned char rx = 60;
+unsigned char ry = 20;
+signed char rdy = 1;
 
 int bucketFreq(int i)
 {
@@ -61,16 +75,84 @@ void adcWriterTask(void *param)
     }
 }
 
+void drawText()
+{
+    videoOut.setFont(&FreeSerif9pt7b);
+    // TV.print(20, 60, "ASTRO BLACK");
+    videoOut.setCursor(50, 160);
+    videoOut.setTextColor(WHITE);
+    videoOut.println("ASTRO BLACK");
+    // if (millis() / 1000 % 2)
+    // {
+    // TV.select_font(font4x6);
+    // TV.print(42, 70, "PRESS START");
+    // }
+}
+
+void drawAsteroids()
+{
+    for (int i = 0; i < ASTEROIDS; i++)
+    {
+        if (asteroidsX[i] <= 0)
+        {
+            asteroidsX[i] = random(240, 340);
+            asteroidsY[i] = random(1, 255);
+            asteroidsZ[i] = random(1, 4);
+        }
+
+        if (asteroidsX[i] <= 240 && asteroidsX[i] > 0)
+            videoOut.fillCircle(asteroidsX[i], asteroidsY[i], asteroidsS[i], WHITE);
+
+        asteroidsX[i] -= asteroidsZ[i];
+    }
+}
+
+void drawRocket()
+{
+    ry += rdy;
+    if (ry > 20)
+        rdy = -1;
+    if (ry < 5)
+        rdy = 1;
+    videoOut.drawBitmap(rx, ry, rocket_img, 72, 40, WHITE);
+}
+
+void rocketScreen()
+{
+    frames++;
+
+    // if (frames > 2)
+    // {
+    videoOut.fillScreen(BLACK);
+    frames = 0;
+    drawRocket();
+    drawText();
+    // }
+
+    drawAsteroids();
+
+    videoOut.waitForFrame();
+}
+
 void setup()
 {
     Serial.begin(115200);
-    esp_wifi_deinit(); // Turn off the WiFi
-    btStop();          // Turn off the BT
-    i2sInit();         // Setup I2S
+    esp_wifi_stop(); // Turn off the WiFi
+    btStop();        // Turn off the BT
+    // i2sInit(); // Setup I2S
+    // xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, NULL, 1, &adcWriterTaskHandle, 1);
 
-    xTaskCreatePinnedToCore(adcWriterTask, "ADC Writer Task", 4096, NULL, 1, &adcWriterTaskHandle, 1);
+    for (int i = 0; i < ASTEROIDS; i++)
+    {
+        asteroidsX[i] = random(1, 239);
+        asteroidsY[i] = random(1, 95);
+        asteroidsZ[i] = random(1, 3);
+        asteroidsS[i] = random(0, 2);
+        ;
+    }
 
     videoOut.begin();
+    Serial.println(3);
 }
 
 void loop()
@@ -79,8 +161,5 @@ void loop()
     unsigned long time = millis();
     float partial_second = (float)(time % 1000) / 1000.0;
 
-    videoOut.waitForFrame();
-
-    // Clear screen
-    videoOut.fillScreen(0);
+    rocketScreen();
 }
---
+++ src/rocket_img.h
@@ -0,0 +1,6 @@
+// #include <avr/pgmspace.h>
+#ifndef ROCKET_IMG_H
+#define ROCKET_IMG_H
+
+extern const unsigned char rocket_img[];
+#endif
