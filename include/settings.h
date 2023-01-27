10985e2b1e79b71625f1d2dce070c2431a37e339
Di M Dub <zensimilia@gmail.com>
2023-01-27 11:05:52 +0300

    Change sampling settings

--- include/settings.h
+++ include/settings.h
@@ -12,14 +12,14 @@
 #define INVERT(color) (0xFF - color)
 
 // Analog to digital
-#define SAMPLING_FREQ 44100
-#define SAMPLES 1024
+#define SAMPLING_FREQ 32000
+#define SAMPLES 512
 #define REAL_SAMPLES (SAMPLES >> 1)
 #define ADC_INPUT ADC1_CHANNEL_0 // GPIO36
 
 // FFT
 #define BANDS 16
-static int16_t bandFreqTable[BANDS] = {50, 80, 100, 150, 250, 400, 650, 800, 1250, 2500, 4000, 6250, 12000, 14000, 16000, 20000};
+static int16_t bandFreqTable[BANDS] = {50, 80, 100, 150, 250, 400, 650, 800, 1250, 2000, 2500, 4000, 6250, 12000, 14000, 16000};
 uint16_t bandBins[BANDS] = {0};
 double_t vReal[SAMPLES];
 double_t vImag[SAMPLES];
