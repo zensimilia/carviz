36d3a920d7794f7e0adecd52f8fd1d78778438e5
Di M Dub <zensimilia@gmail.com>
2023-01-28 00:43:08 +0300

    Tweak i2sInit()

--- include/i2sadc.h
+++ include/i2sadc.h
@@ -13,20 +13,22 @@ void i2sInit()
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
         .sample_rate = SAMPLING_FREQ,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-        .communication_format = I2S_COMM_FORMAT_STAND_MSB,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 4,
+        .dma_buf_count = 8,
         .dma_buf_len = SAMPLES,
         .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0};
 
     // Configuring the I2S driver and pins
+    Serial.printf("Attempting to setup I2S ADC with sampling frequency %d Hz\n", SAMPLING_FREQ);
     i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
     i2s_set_adc_mode(ADC_UNIT_1, ADC_INPUT);
     adc1_config_channel_atten(ADC_INPUT, ADC_ATTEN_DB_11);
     i2s_adc_enable(I2S_PORT);
+    Serial.printf("I2S ADC setup ok\n");
 }
 
 int adcRead(int16_t *samples, int count)
