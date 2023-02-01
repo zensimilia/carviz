7dd7f0c07138e43e156fafe658a68f80406682e0
Di M Dub <zensimilia@gmail.com>
2023-02-01 18:03:36 +0300

    Remove unused extern declarations

--- include/audio_analyzer.h
+++ include/audio_analyzer.h
@@ -6,9 +6,6 @@
 
 #define FFT_READY (1 << 0)
 
-extern QueueHandle_t xSamplesQueue;
-extern EventGroupHandle_t xEventGroup;
-
 void adcReadTask(void *pvParameters);
 void fftComputeTask(void *pvParameters);
 void beginAnalyzerTasks();
