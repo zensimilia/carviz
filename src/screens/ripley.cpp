70a6f5c94b579cc90986cf7ec505e1b2b84e6337
Di M Dub <zensimilia@gmail.com>
2023-02-20 12:54:37 +0300

    Ripley class refactoring

--- src/screens/ripley.cpp
+++ src/screens/ripley.cpp
@@ -4,39 +4,62 @@ using namespace Screens;
 
 void Ripley::draw()
 {
-    _canvas->clear();
+    if (firstDraw)
+        drawBackground();
 
-    _canvas->drawFastHLine(0, 70, _canvas->width(), TFT_WHITE);
-    _canvas->fillRect(0, 72, _canvas->width(), 16, TFT_WHITE);
-    _canvas->drawFastHLine(0, 89, _canvas->width(), TFT_WHITE);
+    drawIndicators();
 
-    _canvas->setFont(&fonts::efontCN_12_b);
-    _canvas->setCursor(_canvas->width() >> 1, 74);
-    _canvas->setTextColor(TFT_BLACK);
-    _canvas->print("体字 サイズ 12 ボルド");
-
-    _canvas->pushImage(30, _canvas->height() - 183, 141, 183, ripley_img, 0xe3);
-
-    _canvas->setFont(&fonts::Font0);
-    _canvas->setCursor(_canvas->width() >> 1, 50);
-    _canvas->setTextColor(TFT_WHITE);
-    _canvas->printf("RAM:%6d", esp_get_free_heap_size());
-
-    drawText();
-
-    _canvas->pushSprite(0, 0);
+    EVERY_N_MILLISECONDS(RIPLEY_WRITE_DELAY_MS)
+    {
+        drawDescription(description);
+    }
 }
 
-void Ripley::drawText()
+void Ripley::drawDescription(const char *text)
 {
+    const size_t descriptionLength = strlen(text);
 
-    if (_di < description.length())
-    {
-        _textarea->print(description.at(_di));
-        _di++;
-    }
-    if (_di >= description.length())
-        _di = 0;
+    if (descriptionIndex < descriptionLength)
+        textarea->print(text[descriptionIndex++]);
 
-    _textarea->pushSprite((_canvas->width() >> 1), (_canvas->height() >> 1) + 5);
+    if (descriptionIndex >= descriptionLength)
+        descriptionIndex = 0;
+
+    textarea->pushSprite((cvbs.width() >> 1), (cvbs.height() >> 1) + 5);
+}
+
+void Ripley::drawIndicators()
+{
+    indicators->clear();
+
+    indicators->setTextColor(TFT_WHITE);
+    indicators->setCursor(0, 0);
+    indicators->printf("RAM:%6d", esp_get_free_heap_size());
+
+    indicators->pushSprite(indicators->width() >> 1, 20);
+}
+
+void Ripley::drawBackground()
+{
+    firstDraw = false;
+
+    cvbs.clear(TFT_BLACK);
+
+    cvbs.drawFastHLine(0, 70, cvbs.width(), TFT_WHITE);
+    cvbs.fillRect(0, 72, cvbs.width(), 16, TFT_WHITE);
+    cvbs.drawFastHLine(0, 89, cvbs.width(), TFT_WHITE);
+
+    cvbs.setTextColor(TFT_BLACK);
+    cvbs.drawString(title, cvbs.width() >> 1, 74, &fonts::efontCN_12_b);
+
+    cvbs.pushImage(30, cvbs.height() - 183, 141, 183, ripley_img, 0xe3);
+}
+
+void Ripley::clearDescription(bool reset)
+{
+    if (reset)
+        descriptionIndex = 0;
+
+    textarea->clear(TFT_BLACK);
+    textarea->setCursor(0, 0);
 }
--- src/screens/ripley.h
+++ src/screens/ripley.h
@@ -2,7 +2,10 @@
 
 #include "lgfx.h"
 #include "aspect.h"
+#include "CEveryNTime.h"
 #include "assets/ripley_img.h"
+
+#define RIPLEY_WRITE_DELAY_MS 500
 
 extern ASpect analyzer; // Global analyzer
 extern LGFX cvbs;       // Global cvbs
@@ -11,37 +14,49 @@ namespace Screens
 {
     class Ripley
     {
-        std::string description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nEtiam vel sem arcu. Etiam pulvinar nec urna ac suscipit. Vestibulum congue bibendum nisi, vel egestas eros varius vel.\n\nNunc lobortis ipsum tortor, vitae tincidunt justo pretium ac. Vivamus dapibus purus vel urna imperdiet, in consequat orci vulputate.\n\n";
+        LGFX_Sprite *indicators;
+        LGFX_Sprite *textarea;
 
-    private:
-        uint32_t *_spectrum = analyzer.getSpectrum();
-        LGFX_Sprite *_canvas;
-        LGFX_Sprite *_textarea;
+        const char *title = "体字 サイズ 12 ボルド";
+        const char *description =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\n"
+            "Etiam vel sem arcu. Etiam pulvinar nec urna ac suscipit. "
+            "Vestibulum congue bibendum nisi, vel egestas eros varius vel.\n\n"
+            "Nunc lobortis ipsum tortor, vitae tincidunt justo pretium ac. "
+            "Vivamus dapibus purus vel urna imperdiet, in consequat orci vulputate.\n\n";
 
-        uint32_t _di = 0;
+        uint32_t *spectrum = analyzer.getSpectrum();
+        uint32_t descriptionIndex = 0;
+
+        bool firstDraw = true;
 
     public:
         Ripley()
         {
-            _canvas = new LGFX_Sprite(&cvbs);
-            _canvas->setColorDepth(lgfx::palette_1bit);
-            _canvas->createSprite(cvbs._width, cvbs._height);
-            _canvas->fillScreen(TFT_BLACK);
+            indicators = new LGFX_Sprite(&cvbs);
+            indicators->setColorDepth(lgfx::palette_1bit);
+            indicators->createSprite(170, 60);
+            indicators->fillScreen(TFT_BLACK);
 
-            _textarea = new LGFX_Sprite(_canvas);
-            _textarea->setColorDepth(lgfx::palette_1bit);
-            _textarea->createSprite(170, 80);
-            _textarea->setClipRect(0, 0, _textarea->width(), _textarea->height());
-            _textarea->setTextWrap(true);
-            _textarea->setTextScroll(true);
+            textarea = new LGFX_Sprite(&cvbs);
+            textarea->setColorDepth(lgfx::palette_1bit);
+            textarea->createSprite(170, 80);
+            textarea->fillScreen(TFT_BLACK);
+            textarea->setClipRect(0, 0, textarea->width(), textarea->height());
+            textarea->setTextWrap(true);
+            textarea->setTextScroll(true);
         };
+        Ripley(const Ripley &) = delete;
         ~Ripley()
         {
-            delete _textarea;
-            delete _canvas;
+            delete textarea;
+            delete indicators;
         };
 
         void draw();
-        void drawText();
+        void drawDescription(const char *text);
+        void drawIndicators();
+        void drawBackground();
+        void clearDescription(bool reset = true);
     }; // class Ripley
 } // namespace Screens
